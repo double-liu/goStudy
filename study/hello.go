674462518433ae3175b16f8b7ca9e0e8800cd712
刘双 <sliu@deMacBook-Pro.local>
2017-07-14 18:02:21 +0800

    no message

--- study/hello.go
+++ study/hello.go
@@ -1,8 +1,63 @@
 // go "hello world" 示例
 package main
- 
-import "fmt"
- 
-func main() {
-    fmt.Printf("Hello, world.\n")
+
+import (
+	"fmt"
+)
+
+var name = "hello word"
+var price = 333.222
+var age int64 = 33
+var a [4]int = [4]int{2, 3, 4, 5}
+
+type   int1  int
+type person struct {
+	name string
+	age  int
+	int1
+	int
 }
+
+var  p  =  person{name:"aa"}
+var zs  person
+type nameBlock func(str string) string
+
+func main() {
+	s := []string{"aa", "bb"}
+	fmt.Printf("slice%d\n", s[0])
+	switch name {
+	case "aa":
+		fmt.Printf("name is aa")
+		fallthrough
+	case "hello word":
+		fmt.Printf("name is hello word")
+	}
+	ll, _ := otherName()
+	fmt.Printf(ll)
+	var more = moreNmae("11", "22", "33")
+	fmt.Printf(more[2])
+
+	block(func(str string) string {
+		return str + "AA"
+	})
+	zs.name = "张三"
+	zs.age  = 33
+	zs.int  = 33
+	zs.int1 = 44
+}
+
+func otherName() (name string, err error) {
+	return "张三", nil
+}
+
+func moreNmae(arg ...string) []string {
+	return arg
+}
+
+func block(block nameBlock) {
+	name := block("block")
+	fmt.Printf(name)
+}
+
+
+
---
+++ study/method.go
@@ -0,0 +1,24 @@
+package main
+import (
+	"fmt"
+	//"strconv"
+	"strconv"
+)
+
+type Human struct {
+	name string
+	age int
+	phone string
+}
+
+// //通过这个方法 Human 实现了 fmt.Stringer
+func (h Human) String() string {
+	return "❰"+h.name+" - "+strconv.Itoa(h.age)+" years -  ✆ " +h.phone+"❱"
+}
+
+func main() {
+	Bob := Human{"Bob", 39, "000-7777-XXX"}
+	fmt.Println("This Human is : ", Bob)
+	
+
+}
